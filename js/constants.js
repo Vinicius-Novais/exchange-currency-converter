c0d53411750202a0c3e3d457df95a4e02f2b74f3
Vinícius <148072300+Vinicius-Novais@users.noreply.github.com>
2026-07-22 09:09:06 -0300

    Create constants.js

---
+++ js/constants.js
@@ -0,0 +1,11 @@
+// prettier-ignore
+export const SUPPORTED_CURRENCIES = [
+  'AED', 'ARS', 'AUD', 'BDT', 'BHD', 'BRL',
+  'CAD', 'CHF', 'CLP', 'CNY', 'COP', 'CZK', 'DKK',
+  'EGP', 'EUR', 'GBP', 'HKD', 'HNL', 'HTG', 'HUF',
+  'IDR', 'INR', 'ISK', 'JOD', 'JPY', 'KES', 'KRW',
+  'KWD', 'LBP', 'LKR', 'MAD', 'MXN', 'MYR', 'NGN',
+  'NOK', 'NPR', 'NZD', 'OMR', 'PEN', 'PHP', 'PKR',
+  'PLN', 'QAR', 'RON', 'RUB', 'SAR', 'SEK', 'SGD',
+  'THB', 'TRY', 'TWD', 'UAH', 'USD', 'VND', 'ZAR'
+];
